cbbdae51b6aeadf1ad3e7acec779da9238217b38
kbpolra <kbpolra@myseneca.ca>
2024-01-17 22:47:08 -0500

    updated all files

--- week 1/OOP workshops/OOP ws1/event.h
+++ week 1/OOP workshops/OOP ws1/event.h
@@ -1,15 +1,21 @@
-#pragma once
 #ifndef SENECA_EVENT_H
 #define SENECA_EVENT_H
-
+#define maxSize 128
 namespace seneca {
 
 	class Event
 	{
+		char eventDescription[maxSize] = "";
+		size_t startingTime{};
 
+	public:
+		Event();
+		void set(const char* desc = nullptr);
+		void display();
 	};
 
+	extern size_t g_sysClock;
 }
 
-#endif // !SDDS_EVENT_H
+#endif // !SENECA_EVENT_H
 
